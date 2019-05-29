21c24acf77931b1dd95d69b926381196c750667d
zabed <zabedalam777@gmail.com>
2019-05-29 16:59:26 +0200

    Updates

--- components/search/getProducts.js
+++
@@ -1,4 +0,0 @@
-const URL = `https://www.unisport.dk/api/products/batch/?list=179249,179838,174351,180011,180020,178429`;
-export function getItems() {
-  return fetch(URL).then(response => response.json());
-}
